6a935ddb29bf12ea5639df83677530680fd26ee1
LENOVO <lalitroushan@gmail.com>
2023-10-24 15:32:44 +0530

    Enhancement for post details

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
 from . import views 
+from .views import PostListView, PostDetailView
 
+# urlpatterns = [
+#     path('', views.home, name='blog-home'),
+#     path('about/', views.about, name='about-page')
+# ]
+ 
+# Replacing views.home with PostListView
 urlpatterns = [
-    path('', views.home, name='blog-home'),
+    path('', PostListView.as_view(), name='blog-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('about/', views.about, name='about-page')
 ]
+
+# <app>/<model>_<viewtype>.html
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
 
@@ -12,3 +13,12 @@ def home(request):
 
 def about(request):
     return render(request, 'blog/about.html')
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html' #<app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted'] # for ordering the post from new to old
+
+class PostDetailView(DetailView):
+    model = Post
