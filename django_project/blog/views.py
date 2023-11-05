c9e6b59a1e4a0e964271b5fb8f4c5aea653d3d32
LENOVO <lalitroushan@gmail.com>
2023-11-06 01:05:03 +0530

    Added pagination

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
+from typing import Any
+from django.db.models.query import QuerySet
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Post
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import (
     LoginRequiredMixin,  # this will redirect the user to sign page if they try localhost/post/new without loging in 
     UserPassesTestMixin) # this won't allow user to update other user's post
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -22,6 +25,18 @@ class PostListView(ListView):
     template_name = 'blog/home.html' #<app>/<model>_<viewtype>.html
     context_object_name = 'posts'
     ordering = ['-date_posted'] # for ordering the post from new to old
+    paginate_by = 4
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog/user_posts.html' #<app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    paginate_by = 4
+
+    def get_queryset(self):   # read more about this function get_queryset
+        user = get_object_or_404(User, username = self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
+
 
 class PostDetailView(DetailView):
     model = Post
